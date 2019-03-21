81b85c7e736bed772aa8dba4757182ccfb459bd0
방문 사용자 <Guest@d7703-29ui-imac.local>
2019-03-21 17:55:49 +0900

    수정본

--- ButtonFun2/ViewController.swift
+++ ButtonFun2/ViewController.swift
@@ -14,14 +14,30 @@ class ViewController: UIViewController {
     
     var count = 0;
     
+    var check = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mylabel.text = "잘되네 히히!"
     }
     @IBAction func btnAdd(_ sender: Any) {
-        labelCount.text = String(count)
-        count += 1;
+        
+        
+        mylabel.text = String(count)
+        if(check == true) {
+            count += 1;
+        }
+        else if (check == false){
+            count -= 1;
+        }
+        
+        if (count == 9){
+            check = false;
+        }
+        else if ( count == 0) {
+            check = true;
+        }
     }
     
 
